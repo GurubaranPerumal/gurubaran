58bcf407d1c539a2880d542bc67b26f2b6a816c5
Ubuntu <ubuntu@ip-172-31-22-163.ec2.internal>
2019-11-05 04:57:45 +0000

    AngularApp

--- src/app/header/header.component.spec.ts
+++ src/app/header/header.component.spec.ts
@@ -5,6 +5,8 @@ import { HeaderComponent } from './header.component';
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
+  let debugElement: any;
+  let element: any;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -21,5 +23,16 @@ describe('HeaderComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('should handle to check header title', () => {
+    debugElement = fixture.debugElement.query(By.css('.mat-toolbar'));
+    if (debugElement) {
+      element = debugElement.nativeElement;
+      expect(element.textContent).toBe('Keep',
+        `should have material toolbar with 'Employee Info' title in header.component.html as <mat-toolbar>Employee Info</mat-toolbar>`);
+    } else {
+      expect(false).toBe(true,
+        `should have an element <mat-toolbar> in your header.component.html to display header title`);
+    }
   });
 });
